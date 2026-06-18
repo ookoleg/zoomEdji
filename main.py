ea1dc8eec8fd0d54dd9751d3d57a8211e7ba5eda
ookoleg <156562052+ookoleg@users.noreply.github.com>
2026-06-18 07:33:30 -0400

    Main python file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import base64
+import re
+import webbrowser
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
@@ -27,21 +30,44 @@ def google_login():
 
             with open(token_file, "w") as token:
                 token.write(creds.to_json())
+# end of google authorization handling
+# Here code is reading through emails to find the ones with zoom link or related to zoom
     try:
         service = build("gmail", "v1", credentials=creds)
-        results = service.users().messages().list(userId="me").execute()
-        print(results)
-        labels = results.get('labels', [])
+        results = service.users().messages().list(userId="me", maxResults=1, q="Zoom").execute()
+        messages = results.get('messages', [])
 
-        if not labels:
-            print("No labels found.")
-            return
-        print("Labels:")
-        for label in labels:
-            print(label["name"])
+        for msg in messages:
+            msg_id = msg['id']
 
+            email = service.users().messages().get(userId="me", id=msg_id).execute()
+            headers = email["payload"]["headers"]
+            body_data = None
+
+            for part in email["payload"].get("parts", []):
+                if part["mimeType"] == "text/plain":        #it got kind of complex here but it was neccesary to make code see link
+                    body_data = part["body"].get("data")
+
+            for header in headers:
+                if header["name"] == "Subject":
+                    print("Subject:", header["value"])
+
+                if header["name"] == "From":
+                    print("From:", header["value"])
+            if body_data:
+                body = base64.urlsafe_b64decode(body_data).decode("utf-8")
+                urls = re.findall(r'https?://[^\s<>"\']+', body)
+                zoom_urls = [url for url in urls if "zoom.us" in url.lower()]
+# at this point it probably found a valid zoom link and began connecting
+                for url in zoom_urls:
+                    print("Zoom link: ", url)
+                    if url:
+                        webbrowser.open(url)
+                        # here it connected to zoom meeting through your default browser
     except HttpError as err:
         print(err)
-#end of google authorization handling
+
 if __name__ == "__main__":
     google_login()
+
+# If you can shorten or simplify my code I would've really like to get feedback on that (I'm still learning)
